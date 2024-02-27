20629d1cfa43c175a290cdf8a68064012955d27b
nekokaito <siddiqsazzad001@gmail.com>
2024-02-27 23:43:12 +0600

    added some js

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,17 @@
-console.log('phone js');
+const loadPhone = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await response.json();
+    const phone = data.data;
+
+    displayPhone(phone);
+}
+
+const displayPhone = phones => {
+   
+   
+
+
+
+}
+
+loadPhone();
